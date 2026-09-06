cde64f626cff179b80698eb820a926c0171a1ce3
laureneisman1833 <laureneisman@gmail.com>
2026-09-06 12:58:00 -0700

    Add function to play wellness club chime sound

---
+++ src/utils/sound.js
@@ -0,0 +1,1 @@
+mport { asset } from './assets' export function playWellnessClubChime() { const audio = new Audio(asset('/sounds/diamond_tunes-harmony-haven-soft-intro-logo-179986.mp3')) audio.volume = 0.6 audio.play().catch(() => { // Autoplay restrictions may block it on some browsers; fails silently }) }
