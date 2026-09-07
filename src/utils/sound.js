5d02f9ebb28820e03d20cd57457811750c72699d
laureneisman1833 <laureneisman@gmail.com>
2026-09-06 22:23:28 -0700

    Format playWellnessClubChime function for readability

--- src/utils/sound.js
+++ src/utils/sound.js
@@ -1,1 +1,9 @@
-import { asset } from './assets' export function playWellnessClubChime() { const audio = new Audio(asset('/sounds/diamond_tunes-harmony-haven-soft-intro-logo-179986.mp3')) audio.volume = 0.6 audio.play().catch(() => { // Autoplay restrictions may block it on some browsers; fails silently }) }
+import { asset } from './assets';
+
+export function playWellnessClubChime() {
+  const audio = new Audio(asset('/sounds/diamond_tunes-harmony-haven-soft-intro-logo-179986.mp3'));
+  audio.volume = 0.6;
+  audio.play().catch(() => {
+    // Autoplay restrictions may block it on some browsers; fails silently
+  });
+}
